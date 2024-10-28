80180f2c968eca31ab8d80cd3b21f03532111604
fabrii2000 <fabriziodemarco2000@gmail.com>
2024-10-28 16:47:12 +0100

    hot fix padding products

--- lib/all_products/widgets/products_grid.dart
+++ lib/all_products/widgets/products_grid.dart
@@ -24,53 +24,56 @@ class _ProductGridState extends State<ProductGrid> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      scrollDirection: Axis.vertical,
-      physics: const NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisSpacing: 8,
-        crossAxisSpacing: 5,
-        // width / height: fixed for all items
-        childAspectRatio: 0.55,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+      child: GridView.builder(
+        scrollDirection: Axis.vertical,
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 0,
+          // width / height: fixed for all items
+          childAspectRatio: 0.525,
+        ),
+        itemCount: widget.pageNumber > 0
+            ? widget.products.length + 2
+            : widget.products.length,
+        itemBuilder: (BuildContext context, int index) {
+          if (index < widget.products.length) {
+
+            // print('PRODUCT PREVIEW');
+            // print(widget.products[index].type);
+
+            return ProductPreview(
+              id: widget.products[index].id ?? 0,  // Default to 0 if id is null
+              image: widget.products[index].images.isEmpty
+                  ? ""  // Default empty string if no image
+                  : widget.products[index].images.first?.src ?? "",  // Null check for src
+              name: widget.products[index].name ?? 'Unknown Product',  // Default name
+              price: widget.products[index].price ?? "0",  // Default price if null
+              regular_price: widget.products[index].regular_price ?? "0",  // Default regular price
+              description: widget.products[index].description ?? 'No Description Available',  // Default description
+              short_description: widget.products[index].short_description ?? 'No Short Description',  // Default short description
+              average_rating: widget.products[index].average_rating ?? "0",  // Default rating
+              tags: widget.products[index].tags ?? [],  // Default to empty list
+              categories: widget.products[index].categories ?? [],  // Default empty list
+              type: widget.products[index].type ?? 'Unknown', productPoint: widget.products[index].points ?? 0,  // Default type if null
+            );
+
+          } else {
+            return const Visibility(
+              visible: false,
+                child: Center(
+                  child: CircularProgressIndicator(
+                    color: Color.fromARGB(255, 161, 29, 51),
+                  ),
+                )
+            );
+          }
+        },
       ),
-      itemCount: widget.pageNumber > 0
-          ? widget.products.length + 2
-          : widget.products.length,
-      itemBuilder: (BuildContext context, int index) {
-        if (index < widget.products.length) {
-
-          // print('PRODUCT PREVIEW');
-          // print(widget.products[index].type);
-
-          return ProductPreview(
-            id: widget.products[index].id ?? 0,  // Default to 0 if id is null
-            image: widget.products[index].images.isEmpty
-                ? ""  // Default empty string if no image
-                : widget.products[index].images.first?.src ?? "",  // Null check for src
-            name: widget.products[index].name ?? 'Unknown Product',  // Default name
-            price: widget.products[index].price ?? "0",  // Default price if null
-            regular_price: widget.products[index].regular_price ?? "0",  // Default regular price
-            description: widget.products[index].description ?? 'No Description Available',  // Default description
-            short_description: widget.products[index].short_description ?? 'No Short Description',  // Default short description
-            average_rating: widget.products[index].average_rating ?? "0",  // Default rating
-            tags: widget.products[index].tags ?? [],  // Default to empty list
-            categories: widget.products[index].categories ?? [],  // Default empty list
-            type: widget.products[index].type ?? 'Unknown', productPoint: widget.products[index].points ?? 0,  // Default type if null
-          );
-
-        } else {
-          return const Visibility(
-            visible: true,
-              child: Center(
-                child: CircularProgressIndicator(
-                  color: Color.fromARGB(255, 161, 29, 51),
-                ),
-              )
-          );
-        }
-      },
     );
   }
 }
